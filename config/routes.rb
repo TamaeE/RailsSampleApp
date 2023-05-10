7694db9125313479ababa2c5a9dd983970ad5c1f
Tamae Ebira <tamae.0727@outlook.jp>
2023-05-10 14:33:12 +0900

    ユーザ認証機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
   root 'static_pages#home'
+  get '/auth/:provider/callback', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
